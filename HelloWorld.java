b54a0e777326f99bcfc07221138755cccda0c3d6
ssomo <78325033+ssomo@users.noreply.github.com>
2021-02-09 14:36:46 -0500

    Update HelloWorld.java
    Added name and age to the program

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,14 @@ public class HelloWorld
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
+      
+      String name = "George Washington";
+      
+      System.out.println(name);
+      
+      int age = 24;
+      
+      System.out.println(age);
+      
    }
 }
