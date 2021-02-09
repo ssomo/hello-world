4715cf1e781a754dcb946d2b2ef1577348d6b9b5
ssomo <78325033+ssomo@users.noreply.github.com>
2021-02-09 14:41:40 -0500

    Revert "Update HelloWorld.java"
    This reverts commit b54a0e777326f99bcfc07221138755cccda0c3d6.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,14 +4,5 @@ public class HelloWorld
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
-      
-      String name = "George Washington";
-      
-      System.out.println(name);
-      
-      int age = 24;
-      
-      System.out.println(age);
-      
    }
 }
